92e0b40ffa609d26601cf76dadf25fcf04630250
Sio Jurnalis Pipin <siojurnalispipin@hotmail.com>
2019-03-12 15:38:13 +0700

    ganti dari client ke serve

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 const express = require('express')
 const app = express()
-
-
 //Menggunakan view engine ejs
 app.set('view engine', 'ejs')
 
 //middlewarenya
 app.use(express.static('public'))
-
-
 //routes
 app.get('/', (req, res) => {
 	res.render('index')
@@ -16,8 +12,6 @@ app.get('/', (req, res) => {
 
 //Listen menggunakan port 3000
 server = app.listen(3000)
-
-
 
 //instantitation atau pembuatan object spesifik socket.io 
 const io = require("socket.io")(server)
@@ -37,8 +31,9 @@ io.on('connection', (socket) => {
 
     //listen on Pesan baru coek
     socket.on('new_message', (data) => {
-        //broadcast the new message
-        io.sockets.emit('new_message', {message : data.message, username : socket.username});
+        // //broadcast the new message
+        // io.sockets.emit('new_message', {message : data.message, username : socket.username});
+        console.log(data)
     })
 
     //listen ketika melakukan typing
--- public/chat.js
+++ public/chat.js
@@ -41,28 +41,67 @@ $(function(){
 	
 
 	//Timbang
-	var message = $("#message")
+	// var message = $("#message")
+	// var send_message = $("#send_message")
+	// var chatroom = $("#chatroom")
+	// var feedback = $("#feedback")
+
+	// //Emit message
+	// send_message.click(function(){
+	// 	socket.emit('new_message', {message : message.val()})
+	// })
+
+	// //Listen on new_message
+	// socket.on("new_message", (data) => {
+	// 	feedback.html('');
+	// 	message.val('');
+	// 	chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
+	// })
+
+	// //Emit a username
+	// send_username.click(function(){
+	// 	socket.emit('change_username', {username : username.val()})
+	// })
+
+	//timbang 2
+	var message = 8000
+	var lama = 4
+	var interval = 200
 	var send_message = $("#send_message")
 	var chatroom = $("#chatroom")
 	var feedback = $("#feedback")
+	var display = $("#display")
 
 	//Emit message
 	send_message.click(function(){
-		socket.emit('new_message', {message : message.val()})
+		msg = parseFloat(message)
+		var waktu = parseFloat(lama) * 1000
+		display.innerText = 0
+		var start = new Date()
+		var angkaInterval = setInterval(() => {
+			var skrg = new Date()
+			var selisiWaktu = skrg - start
+			var persentase = selisiWaktu / waktu
+			var hasil = lerp(0, message, persentase)
+			 console.log(hasil)
+			socket.emit('new_message', hasil)
+			if(persentase > 1) clearInterval(angkaInterval)
+		}, interval);
 	})
+
+	var lerp = function(dari, ke, n){ // n diisi dengan nilai 0 s/d 1
+		if (n > 1) return ke
+		else if (n < 0) return dari
+		return dari + (ke - dari) * n
+	  }
 
 	//Listen on new_message
 	socket.on("new_message", (data) => {
-		feedback.html('');
-		message.val('');
-		chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
+		
+		console.log(data)
+		$('#display').append($('<li>').text(data.hasil));
+		display.innerText = data.hasil 
 	})
-
-	//Emit a username
-	send_username.click(function(){
-		socket.emit('change_username', {username : username.val()})
-	})
-
 });
 
 
